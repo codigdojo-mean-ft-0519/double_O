4995ddb6d4c8048f68a348774cdbd38c568bb40d
Oladimeji Oladipo <jedi132000@gmail.com>
2019-05-22 21:03:22 -0400

    restful-api-interactive

---
+++ 05-angular/03-dom-manipulation/restful-task-interactive/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { Task } from './task.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'restfulDom';
+  tasks: Task[];
+  task: Task;
+  selectedTask: Task;
+
+  constructor(private _httpService: HttpService) {}
+
+  buttonGetAllTasks() {
+    const observable: Observable<Task[]> = this._httpService.getTasks();
+    observable.subscribe(data => {
+      this.tasks = data;
+      console.log('Got all tasks from the click! Our tasks are', this.tasks);
+    });
+  }
+  buttonShowOneTask(clickedTask: Task) {
+    console.log('Our clicked task is...', clickedTask);
+    //                            (expression)      ? (if true) : (if false);
+    this.selectedTask = this.selectedTask == clickedTask ? null : clickedTask;
+  }
+
+  getTask(id: string) {
+    this._httpService.getTask(id).subscribe(task => {
+      this.task = task;
+    });
+  }
+}
+
+
+
